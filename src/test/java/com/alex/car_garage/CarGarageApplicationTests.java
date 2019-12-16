284e0df7f9de47f4cc5b792ef646f5cba73cf21a
AlexBobryshev <darknessrider1@rambler.ru>
2019-12-17 00:08:11 +0300

    init commit

---
+++ src/test/java/com/alex/car_garage/CarGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.car_garage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGarageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
