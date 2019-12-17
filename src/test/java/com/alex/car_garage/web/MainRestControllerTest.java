aca00e2af3c79b7a89f4d0c88731884da75d15a0
AlexBobryshev <darknessrider1@rambler.ru>
2019-12-18 00:21:15 +0300

    tests are in process

--- src/test/java/com/alex/car_garage/web/MainRestControllerTest.java
+++ src/test/java/com/alex/car_garage/web/MainRestControllerTest.java
@@ -7,10 +7,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,22 +30,35 @@ public class MainRestControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void testGetCars() {
+    public void testGetCars() throws Exception {
+        mockMvc.perform(get("/cars"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void testGetCar() {
+    public void testGetCar() throws Exception {
+        mockMvc.perform(get("/car/-1"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void testAddCar() {
+    public void testAddCar() throws Exception {
+        mockMvc.perform(post("/car")
+                //.param("carForm", ))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{model:'Toyota', color:'BLACK'}"))
+                .andDo(print())
+                .andExpect(status().isOk());
+                //.andExpect(content().string(containsString("\"points\":11")));
     }
 
     @Test
-    public void testUpdateCar() {
+    public void testUpdateCar() throws Exception {
     }
 
     @Test
-    public void testDeleteCar() {
+    public void testDeleteCar() throws Exception {
     }
 }
